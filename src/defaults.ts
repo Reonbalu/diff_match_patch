7a03d9cda3b95d595e33aeaf89eadef2430d40be
Reonbalu <balu.reon@gmail.com>
2021-03-14 08:56:05 +0900

    save

--- src/defaults.ts
+++ src/defaults.ts
@@ -5,6 +5,6 @@ const oldText = `Title Register: ABC123 - Registered at: 2019-12-24
 const newText = `Title Register: ABC123 - Registered at: 2019-12-24
 
 長時間労働が原因で自殺したとして昨年末に労災認定された東芝のグループ会社のシステムエンジニア、安部真生（しんば）さん（当時３０）は２０１９年６月以降、介護に関するシステムの構築に従事し、忙しい日々を送っていた。１５年に入社し、健康保険のシステムの運用・保守を担当。19年4月に川崎市の本社に異動するまでシステム開発の経験はなかったが、システム構築の中心的な役割を任されていた。
-NOTE: The tenant is under an obligation to offer to surrender the lease in the circumstances therein mentioned.`;
+NOTE:The tenant is under an obligation to offer to surrender the lease in the circumstances therein mentioned.`;
 
 export { oldText, newText };
